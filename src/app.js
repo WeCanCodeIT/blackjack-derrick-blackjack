46adcc0f078721779218627f90d786dc142bb741
Zack Kramer <zckramer83@gmail.com>
2019-10-21 14:39:46 -0400

    added linting and prettier

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 // console.log('Hello world');
 
-const buttons = document.querySelectorAll(".button");
+const buttons = document.querySelectorAll('.button')
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const paragraph = document.createElement("p");
-        paragraph.textContent = "My new paragraph!";
+  button.addEventListener('click', () => {
+    const paragraph = document.createElement('p')
+    paragraph.textContent = 'My new paragraph!'
 
-        paragraph.classList.add("newParagraph");
+    paragraph.classList.add('newParagraph')
 
-        document.body.append(paragraph);    
-    });
-});
-const backgroundButton = document.querySelector(".addBackground");
-backgroundButton.addEventListener("click", () => {
-    paragraph.querySelectorAll("newParagraph");
-
+    document.body.append(paragraph)
+  })
+})
+const backgroundButton = document.querySelector('.addBackground')
+backgroundButton.addEventListener('click', () => {
+  paragraph.querySelectorAll('newParagraph')
+  paragraph.classList.add('newParagraph')
 })
